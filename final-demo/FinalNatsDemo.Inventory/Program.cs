0b7f63d8a4e188f1d6ee74bea234558d299c63fc
Henry Ihenacho <64085210+henrychris@users.noreply.github.com>
2024-09-16 23:16:32 +0100

    init final demo

---
+++ final-demo/FinalNatsDemo.Inventory/Program.cs
@@ -0,0 +1,7 @@
+using FinalNatsDemo.Inventory;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
---
+++ final-demo/FinalNatsDemo.Shipping/Program.cs
@@ -0,0 +1,7 @@
+using FinalNatsDemo.Shipping;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
