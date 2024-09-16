9598c1c10942a514a2ba7932dabe42300ace0f23
Henry Ihenacho <64085210+henrychris@users.noreply.github.com>
2024-09-16 17:55:09 +0100

    implement publisher & subscriber

--- 1-pub-sub-demo/NatsPubSubDemo.Publisher/Program.cs
+++ 1-pub-sub-demo/NatsPubSubDemo.Publisher/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using NATS.Client.Core;
+using NatsPubSubDemo.Common;
+
+var opts = new NatsOpts { Url = NatsConfig.DefaultUrl };
+await using var connection = new NatsConnection(opts);
+var messageCount = 0;
+
+while (true)
+{
+    var message = new Message
+    {
+        Id = Guid.NewGuid().ToString(),
+        Content = $"Message #{messageCount} at {DateTime.Now}",
+        Timestamp = DateTime.Now,
+    };
+
+    Console.WriteLine("Hit any key to publish a message.");
+    var key = Console.ReadKey(true);
+
+    if (key.Key == ConsoleKey.Escape) // If Escape is pressed, you can break the loop if needed
+    {
+        return;
+    }
+
+    await connection.PublishAsync(
+        NatsConfig.SubjectName,
+        message,
+        serializer: new NatsSerializer<Message>()
+    );
+
+    Console.WriteLine($"Published message #{messageCount}.");
+    messageCount++;
+}
--- 1-pub-sub-demo/NatsPubSubDemo.Subscriber/Subscriber.cs
+++ 1-pub-sub-demo/NatsPubSubDemo.Subscriber/Subscriber.cs
@@ -1,16 +1,40 @@
+using NATS.Client.Core;
+using NatsPubSubDemo.Common;
+
 namespace NatsPubSubDemo.Subscriber;
 
 public class Subscriber(ILogger<Subscriber> logger) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        var opts = new NatsOpts { Url = NatsConfig.DefaultUrl };
+        await using var connection = new NatsConnection(opts);
+
         while (!stoppingToken.IsCancellationRequested)
         {
-            if (logger.IsEnabled(LogLevel.Information))
+            logger.LogInformation("Subscribing to topic: {topic}.", NatsConfig.SubjectName);
+            await foreach (
+                var msg in connection.SubscribeAsync<Message>(
+                    NatsConfig.SubjectName,
+                    serializer: new NatsSerializer<Message>(),
+                    cancellationToken: stoppingToken
+                )
+            )
             {
-                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                Console.WriteLine($"Received {msg.Subject}: {msg.Data}\n");
+
+                if (msg.Data is null)
+                {
+                    logger.LogError("Message is null.");
+                    continue;
+                }
+
+                logger.LogInformation(
+                    "Received: {messageId} - {message.Content}",
+                    msg.Data.Id,
+                    msg.Data.Content
+                );
             }
-            await Task.Delay(1000, stoppingToken);
         }
     }
 }
