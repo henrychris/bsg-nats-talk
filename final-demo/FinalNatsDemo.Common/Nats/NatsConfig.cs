762e07ad731a322ffe6ebf8bf84da4406e8d65aa
Henry Ihenacho <64085210+henrychris@users.noreply.github.com>
2024-09-19 17:39:20 +0100

    add nats common classes

---
+++ final-demo/FinalNatsDemo.Common/Nats/NatsConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace FinalNatsDemo.Common.Nats;
+
+public static class NatsConfig
+{
+    public const string DefaultUrl = "nats://localhost:4222";
+    public const string SubjectName = "demo.messages.3";
+    public const string StreamName = "demostream-3";
+}
---
+++ final-demo/FinalNatsDemo.Common/Nats/Setup.cs
@@ -0,0 +1,19 @@
+using Microsoft.Extensions.DependencyInjection;
+using NATS.Client.Core;
+using NATS.Client.JetStream;
+
+namespace FinalNatsDemo.Common.Nats
+{
+    public static class Setup
+    {
+        public static void AddNats(this IServiceCollection services)
+        {
+            var natsOpts = new NatsOpts { Url = NatsConfig.DefaultUrl };
+
+            var natsConnection = new NatsConnection(natsOpts);
+            services.AddSingleton<INatsConnectionPool>(new NatsConnectionPool(natsOpts));
+            services.AddSingleton(new NatsJSContext(natsConnection, new NatsJSOpts(natsOpts)));
+            services.AddScoped<INatsWrapper, NatsWrapper>();
+        }
+    }
+}
