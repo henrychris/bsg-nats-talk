eec3a3c2ab67d17470364e8e4de6f70ccf8a17a8
henrychris <64085210+henrychris@users.noreply.github.com>
2024-09-29 16:51:03 +0100

    add subscriber

--- 4-queue-group-demo/QueueGroupDemo.Consumers/Program.cs
+++ 4-queue-group-demo/QueueGroupDemo.Consumers/Program.cs
@@ -4,6 +4,7 @@ var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddHostedService<Subscriber>();
 builder.Services.AddHostedService<Subscriber2>();
+// builder.Services.AddHostedService<Subscriber3>();
 
 var host = builder.Build();
 host.Run();
---
+++ 4-queue-group-demo/QueueGroupDemo.Consumers/Subscriber3.cs
@@ -0,0 +1,57 @@
+using NATS.Client.Core;
+using QueueGroupDemo.Common;
+using QueueGroupDemo.Common.Nats;
+
+namespace QueueGroupDemo.Consumers
+{
+    public class Subscriber3(ILogger<Subscriber3> logger) : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var opts = new NatsOpts { Url = NatsConfig.DefaultUrl, Name = "Subscriber 2" };
+            await using var nats = new NatsConnection(opts);
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    var subscription = nats.SubscribeAsync<Message>(
+                        NatsConfig.SubjectName,
+                        queueGroup: "subscriber-queue",
+                        serializer: new NatsSerializer<Message>(),
+                        cancellationToken: stoppingToken
+                    );
+
+                    await foreach (var msg in subscription)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        if (msg.Data is null)
+                        {
+                            continue;
+                        }
+
+                        Console.WriteLine(
+                            $"{nameof(Subscriber3)} received message on subject: {msg.Subject}.\nReceived: {msg.Data.Id} - {msg.Data.Content}"
+                        );
+
+                        Console.ResetColor();
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    logger.LogInformation("Cancellation requested, shutting down gracefully.");
+                }
+                catch (Exception e)
+                {
+                    logger.LogError(
+                        "Something went wrong in the handler for {subject}.\nDetails: {exception}",
+                        NatsConfig.SubjectName,
+                        e
+                    );
+                }
+            }
+        }
+    }
+}
